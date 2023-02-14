2250230f0802d7d636c5b405e7a71076047b12b6
Barry <barrycumbie@gmail.com>
2023-02-14 09:32:23 -0600

    init node expressw hello app

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+const PORT = process.env.var || 3000; 
+
+app.get('/', function (req, res) {
+  res.send('Hello World with Express on The Web !')
+})
+
+console.log("hello from node server");
+
+app.listen(PORT); 
+
