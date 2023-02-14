c0d0e5f498aff7cfb52a1de123f6f948c871fcc2
barry-cumbie <barrycumbie@gmail.com>
2023-02-14 09:52:28 -0600

    Update app.js

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const app = express()
 
 const PORT = process.env.PORT || 3000; 
 
+
 app.get('/', function (req, res) {
   res.send('Hello World with Express on The Web !')
 })
 
 console.log("hello from node server");
 
-app.listen(PORT); 
-
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${ PORT }`)
+})
